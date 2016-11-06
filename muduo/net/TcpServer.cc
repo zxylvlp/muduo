7a477d861c464bef59558f169806b9b5650d617c
金叹 <jintan.zxy@alibaba-inc.com>
2016-11-06 23:16:01 +0800

    add

--- muduo/net/TcpServer.cc
+++ muduo/net/TcpServer.cc
@@ -19,6 +19,12 @@
 using namespace muduo;
 using namespace muduo::net;
 
+/**
+ * 构造函数
+ *
+ * 做初始化工作，这里构建了acceptor和EventLoop线程池
+ * 并且给acceptor设置了新建连接回调函数为TcpServer::newConnection
+ */
 TcpServer::TcpServer(EventLoop* loop,
                      const InetAddress& listenAddr,
                      const string& nameArg,
@@ -36,6 +42,11 @@ TcpServer::TcpServer(EventLoop* loop,
       std::bind(&TcpServer::newConnection, this, _1, _2));
 }
 
+/**
+ * 析构函数
+ *
+ * 迭代连接map中的连接，将其连接指针置为空，并且将连接在其loop中调用TcpConnection::connectDestroyed函数
+ */
 TcpServer::~TcpServer()
 {
   loop_->assertInLoopThread();
@@ -52,12 +63,22 @@ TcpServer::~TcpServer()
   }
 }
 
+/**
+ * 设置EventLoop线程池的线程数
+ */
 void TcpServer::setThreadNum(int numThreads)
 {
   assert(0 <= numThreads);
   threadPool_->setThreadNum(numThreads);
 }
 
+/**
+ * 开启服务器
+ *
+ * 如果还没开启则做下面的操作：
+ * 首先调用EventLoop线程池的开启函数，并且传入线程初始化回调函数
+ * 并且在loop线程调用acceptor的listen函数
+ */
 void TcpServer::start()
 {
   if (started_.getAndSet(1) == 0)
@@ -70,6 +91,15 @@ void TcpServer::start()
   }
 }
 
+/**
+ * 新建连接时的处理函数
+ *
+ * 从EventLoop线程池中获得一个loop，并且根据服务器名、ip地址和端口号还有下一个连接id创建一个连接名
+ * 将下一个连接id加1
+ * 利用前面的信息创建一个连接对象，并且把连接对象包裹在智能指针中，利用连接名作为key，将智能指针放到连接map中
+ * 设置连接的建立连接回调函数、收到消息回调函数、写完成回调函数和关闭连接回调函数
+ * 最后在loop线程里调用TcpConnection::connectEstablished函数
+ */
 void TcpServer::newConnection(int sockfd, const InetAddress& peerAddr)
 {
   loop_->assertInLoopThread();
@@ -99,12 +129,22 @@ void TcpServer::newConnection(int sockfd, const InetAddress& peerAddr)
   ioLoop->runInLoop(std::bind(&TcpConnection::connectEstablished, conn));
 }
 
+/**
+ * 删除连接函数
+ *
+ * 在服务器loop线程调用TcpServer::removeConnectionInLoop
+ */
 void TcpServer::removeConnection(const TcpConnectionPtr& conn)
 {
   // FIXME: unsafe
   loop_->runInLoop(std::bind(&TcpServer::removeConnectionInLoop, this, conn));
 }
 
+/**
+ * loop线程删除连接函数
+ *
+ * 将连接从连接map中删去，然后获得连接的loop，在其线程中调用TcpConnection::connectDestroyed
+ */
 void TcpServer::removeConnectionInLoop(const TcpConnectionPtr& conn)
 {
   loop_->assertInLoopThread();
--- muduo/net/TcpServer.h
+++ muduo/net/TcpServer.h
@@ -30,10 +30,19 @@ class EventLoopThreadPool;
 /// TCP server, supports single-threaded and thread-pool models.
 ///
 /// This is an interface class, so don't expose too much details.
+/**
+ * TCP服务器类
+ */
 class TcpServer : noncopyable
 {
  public:
+  /**
+   * 线程初始化回调函数类型，此函数返回值为void，参数为EventLoop指针
+   */
   typedef std::function<void(EventLoop*)> ThreadInitCallback;
+  /**
+   * 指定是否重用处于time_wait的端口的选项
+   */
   enum Option
   {
     kNoReusePort,
@@ -47,8 +56,17 @@ class TcpServer : noncopyable
             Option option = kNoReusePort);
   ~TcpServer();  // force out-line dtor, for std::unique_ptr members.
 
+  /**
+   * 返回ip地址和端口号
+   */
   const string& ipPort() const { return ipPort_; }
+  /**
+   * 返回服务器的名字
+   */
   const string& name() const { return name_; }
+  /**
+   * 返回指向EventLoop的指针
+   */
   EventLoop* getLoop() const { return loop_; }
 
   /// Set the number of threads for handling input.
@@ -62,9 +80,15 @@ class TcpServer : noncopyable
   /// - N means a thread pool with N threads, new connections
   ///   are assigned on a round-robin basis.
   void setThreadNum(int numThreads);
+  /**
+   * 设置线程初始化回电函数
+   */
   void setThreadInitCallback(const ThreadInitCallback& cb)
   { threadInitCallback_ = cb; }
   /// valid after calling start()
+  /**
+   * 返回指向EventLoop线程池的智能指针
+   */
   std::shared_ptr<EventLoopThreadPool> threadPool()
   { return threadPool_; }
 
@@ -76,16 +100,25 @@ class TcpServer : noncopyable
 
   /// Set connection callback.
   /// Not thread safe.
+  /**
+   * 连接成功回调函数
+   */
   void setConnectionCallback(const ConnectionCallback& cb)
   { connectionCallback_ = cb; }
 
   /// Set message callback.
   /// Not thread safe.
+  /**
+   * 设置收到消息回调函数
+   */
   void setMessageCallback(const MessageCallback& cb)
   { messageCallback_ = cb; }
 
   /// Set write complete callback.
   /// Not thread safe.
+  /**
+   * 设置写完成回调函数
+   */
   void setWriteCompleteCallback(const WriteCompleteCallback& cb)
   { writeCompleteCallback_ = cb; }
 
@@ -97,20 +130,59 @@ class TcpServer : noncopyable
   /// Not thread safe, but in loop
   void removeConnectionInLoop(const TcpConnectionPtr& conn);
 
+  /**
+   * 定义ConnectionMap类型，他是从字符串到指向TcpConnection智能指针的映射
+   */
   typedef std::map<string, TcpConnectionPtr> ConnectionMap;
 
+  /**
+   * 指向EventLoop的指针
+   */
   EventLoop* loop_;  // the acceptor loop
+  /**
+   * 服务器的ip地址加端口号
+   */
   const string ipPort_;
+  /**
+   * 服务器的名称
+   */
   const string name_;
+  /**
+   * 指向Acceptor的唯一指针
+   */
   std::unique_ptr<Acceptor> acceptor_; // avoid revealing Acceptor
+  /**
+   * 指向EventLoop线程池的智能指针
+   */
   std::shared_ptr<EventLoopThreadPool> threadPool_;
+  /**
+   * 连接成功回调函数
+   */
   ConnectionCallback connectionCallback_;
+  /**
+   * 收到消息回调函数
+   */
   MessageCallback messageCallback_;
+  /**
+   * 写完成回调函数
+   */
   WriteCompleteCallback writeCompleteCallback_;
+  /**
+   * 线程初始化回调函数
+   */
   ThreadInitCallback threadInitCallback_;
+  /**
+   * 服务器是否已经启动
+   */
   AtomicInt32 started_;
   // always in loop thread
+  /**
+   * 下一个连接的id
+   */
   int nextConnId_;
+  /**
+   * 存放连接的map
+   */
   ConnectionMap connections_;
 };
 
